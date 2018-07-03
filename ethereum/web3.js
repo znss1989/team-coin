1639a77055468b71bfbd94ff14be55626d4494ff
znss1989 <znss1989@gmail.com>
2018-07-03 10:15:12 +0800

    server and browser web3

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,11 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+if (window && window.web3) { // in browser and has metamask
+  web3 = new Web3(window.web3.currentProvider);
+} else { // server or no metamask
+  const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/QMjlpARxJBrcDKChTfPB');
+  web3 = new Web3(provider);
+}
 
 export default web3;
