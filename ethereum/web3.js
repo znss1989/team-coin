98560b9777fe802d86fe033b835c98b328cfa54b
znss1989 <znss1989@gmail.com>
2018-07-03 10:17:18 +0800

    hot fix

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 
 let web3;
-if (window && window.web3) { // in browser and has metamask
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') { // in browser and has metamask
   web3 = new Web3(window.web3.currentProvider);
 } else { // server or no metamask
   const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/QMjlpARxJBrcDKChTfPB');
