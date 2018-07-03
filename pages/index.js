1d2ae9594986317c9ba130163310b25f33792c57
znss1989 <znss1989@gmail.com>
2018-07-03 11:27:52 +0800

    style with semantic ui

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Card, Button} from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignsIndex extends React.Component {
@@ -9,9 +10,25 @@ class CampaignsIndex extends React.Component {
     };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View campaign</a>,
+        fluid: true
+      };
+    });
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
-      <div>CampaignsIndex</div>
+      <div>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+        <h1>Team Coin</h1>
+        {this.renderCampaigns()}
+        <Button content="Create Campaign" icon="add" primary />
+      </div>
     );
   }
 }
