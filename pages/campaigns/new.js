005a4704ab67d57dd9405e535497c89b1c8f4066
znss1989 <znss1989@gmail.com>
2018-07-04 09:44:35 +0800

    loading ui

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,32 +1,59 @@
 import React from 'react';
-import {Form, Input, Button} from 'semantic-ui-react';
+import {Form, Input, Button, Message} from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends React.Component {
   constructor() {
     super();
     this.state = {
-      minimumContribution: ''
+      minimumContribution: '',
+      errMessage: '',
+      loading: false
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({
-      minimumContribution: event.target.value
+      minimumContribution: event.target.value,
     });
   }
 
-  onSubmit() {
-    
+  async onSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+      errMessage: false
+    });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(this.state.minimumContribution).send({
+        from: accounts[0]
+      });
+    } catch (err) {
+      this.setState({
+        errMessage: err.message
+      });
+    }
+    this.setState({
+      loading: false
+    });
   }
 
   render() {
     return (
       <Layout>
         <h3>Create A Campaign</h3>
-        <Form>
+        <Form 
+          onSubmit={event => {
+            this.onSubmit(event);
+          }}
+          error={!!this.state.errMessage}
+        >
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -37,7 +64,8 @@ class CampaignNew extends React.Component {
               }} 
             />
           </Form.Field>
-          <Button primary>Create</Button>
+          <Message error header="Oops" content={this.state.errMessage}/>
+          <Button primary loading={this.state.loading}>Create</Button>
         </Form>
       </Layout>
     );
