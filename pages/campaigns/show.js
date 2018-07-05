46d8044596e2bfa44992ce6be34187c47cce71df
znss1989 <znss1989@gmail.com>
2018-07-05 11:02:56 +0800

    get summary items from campaign

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -5,13 +5,17 @@ import getCampaignAt from '../../ethereum/campaign';
 class CampaignShow extends React.Component {
   static async getInitialProps(props) {
     const campaign = getCampaignAt(props.query.address);
+    const summary = campaign.methods.getSummary().call();
     return {
-
+      minimumContribution: summary['0'],
+      balance: summary['1'],
+      requestCount: summary['2'],
+      approversCount: summary['3'],
+      manager: summary['4'],
     };
   }
 
   render() {
-    console.log("Haha");
     return (
       <Layout>
         <h3>CampaignShow</h3>
