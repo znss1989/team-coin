bfae6efd1a8f0acf379033ce14273b40814198ed
znss1989 <znss1989@gmail.com>
2018-07-06 19:40:25 +0800

    prepare requests list in props

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,11 +3,19 @@ import {Button} from 'semantic-ui-react';
 
 import {Link} from '../../../routes';
 import Layout from '../../../components/Layout';
+import getCampaignAt from '../../../ethereum/campaign';
 
 class RequestIndex extends React.Component {
   static async getInitialProps(props) {
     const {address} = props.query;
-    return {address};
+    const campaign = getCampaignAt(address);
+    const requestsCount = await campaign.methods.getRequestCount().call();
+    const requests = await Promise.all(
+      Array(parseInt(requestCount)).fill().map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+    );
+    return {address, requests, requestsCount};
   }
 
   render() {
