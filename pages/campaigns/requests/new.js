c9487a2e25a1d79b12a68a3685c7c2b3f76376ec
znss1989 <znss1989@gmail.com>
2018-07-06 16:32:20 +0800

    link to request new

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Form, Button, Message} from 'semantic-ui-react';
+
+class RequestNew extends React.Component {
+  render() {
+    return (
+      <div>Request new</div>
+    );
+  }
+}
+
+export default RequestNew;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -68,17 +68,23 @@ class CampaignShow extends React.Component {
       <Layout>
         <h3>CampaignShow</h3>
         <Grid>
-          <Grid.Column width={10}>
-            {this.renderCards()}
-            <Link route={`/campaigns/${this.props.address}/requests`}>
-              <a>
-                <Button primary>View Requests</Button>
-              </a>
-            </Link>
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <ContributeForm address={this.props.address} />
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              {this.renderCards()}
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <ContributeForm address={this.props.address} />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Link route={`/campaigns/${this.props.address}/requests`}>
+                <a>
+                  <Button primary>View Requests</Button>
+                </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </Layout>
     );
