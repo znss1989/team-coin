0db5691df6c6858a2054d5793b46f27509fa1048
znss1989 <znss1989@gmail.com>
2018-07-09 19:30:32 +0800

    request row polish

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -24,18 +24,27 @@ class RequestRow extends React.Component {
 
   render() {
     const {id, request, approversCount} = this.props;
+    const readyToFinalize = request.approvalCount > approversCount / 2;
     return (
-      <Table.Row>
+      <Table.Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
         <Table.Cell>{id}</Table.Cell>
         <Table.Cell>{request.description}</Table.Cell>
         <Table.Cell>{web3.utils.fromWei(request.value, 'ether')}</Table.Cell>
         <Table.Cell>{request.recipient}</Table.Cell>
         <Table.Cell>{request.approvalCount}/{approversCount}</Table.Cell>
         <Table.Cell>
-          <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+          {
+            request.complete ? null : (
+              <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+            )
+          }
         </Table.Cell>
         <Table.Cell>
-          <Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+          {
+            request.complete ? null : (
+              <Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+            )
+          }
         </Table.Cell>
       </Table.Row>
     );
