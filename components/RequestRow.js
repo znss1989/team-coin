fad4b61f8120c25baa05b9780fd947301411d286
znss1989 <znss1989@gmail.com>
2018-07-09 19:13:11 +0800

    request rows

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import {Table, Button} from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
+import getCampaignAt from '../ethereum/campaign';
 
 class RequestRow extends React.Component {
+  constructor() {
+    super();
+    this.onApprove = this.onApprove.bind(this);
+    this.onFinalize = this.onFinalize.bind(this);
+  }
+
+  async onApprove() {
+    const campaign = getCampaignAt(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(this.props.id).send({from: accounts[0]});
+  }
+
+  async onFinalize() {
+    const campaign = getCampaignAt(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(this.props.id).send({from: accounts[0]});
+  }
+
   render() {
+    const {id, request, approversCount} = this.props;
     return (
-      <div>Request Row</div>
+      <Table.Row>
+        <Table.Cell>{id}</Table.Cell>
+        <Table.Cell>{request.description}</Table.Cell>
+        <Table.Cell>{web3.utils.fromWei(request.value, 'ether')}</Table.Cell>
+        <Table.Cell>{request.recipient}</Table.Cell>
+        <Table.Cell>{request.approvalCount}/{approversCount}</Table.Cell>
+        <Table.Cell>
+          <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+        </Table.Cell>
+        <Table.Cell>
+          <Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+        </Table.Cell>
+      </Table.Row>
     );
   }
 }
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -4,18 +4,28 @@ import {Button, Table} from 'semantic-ui-react';
 import {Link} from '../../../routes';
 import Layout from '../../../components/Layout';
 import getCampaignAt from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends React.Component {
   static async getInitialProps(props) {
     const {address} = props.query;
     const campaign = getCampaignAt(address);
     const requestCount = await campaign.methods.getRequestCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
     const requests = await Promise.all(
       Array(parseInt(requestCount)).fill().map((element, index) => {
         return campaign.methods.requests(index).call();
       })
     );
-    return {address, requests, requestCount};
+    return {address, requests, requestCount, approversCount};
+  }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+          <RequestRow key={index} id={index} request={request} address={this.props.address} approversCount={this.props.approversCount}/>
+      );
+    });
   }
 
   render() {
@@ -32,13 +42,16 @@ class RequestIndex extends React.Component {
             <Table.Row>
               <Table.HeaderCell>ID</Table.HeaderCell>
               <Table.HeaderCell>Description</Table.HeaderCell>
-              <Table.HeaderCell>Amount</Table.HeaderCell>
+              <Table.HeaderCell>Amount (ether)</Table.HeaderCell>
               <Table.HeaderCell>Recipient</Table.HeaderCell>
               <Table.HeaderCell>Approval Count</Table.HeaderCell>
               <Table.HeaderCell>Approve</Table.HeaderCell>
               <Table.HeaderCell>Finalize</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
+          <Table.Body>
+            {this.renderRows()}
+          </Table.Body>
         </Table>
       </Layout>
     );
